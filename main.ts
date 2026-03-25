37817487d939972c49e8e81a68cf1870fc9f5c52
jensonmercieca-wq <jensonmercieca@gmail.com>
2026-03-25 12:01:39 +0000

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,7 @@
+input.onButtonPressed(Button.B, function () {
+    cuteBot.setServo(cuteBot.ServoList.S2, 60)
+    cuteBot.colorLight(cuteBot.RGBLights.ALL, 0xb09eff)
+})
+basic.forever(function () {
+	
+})
